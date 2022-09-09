ec030ee9a8c49e1cf0c781d1cbc9ded8c29417d0
GT <46331028+ygyzys83@users.noreply.github.com>
2022-09-09 13:10:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,115 +1,94 @@
 from tkinter import *
 import math
-
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
-clock_timer = None
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- #
+# ---------------------------- TIMER RESET ------------------------------- # 
 
 
 def reset_timer():
-    window.after_cancel(clock_timer)
+    window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
-    timer.configure(text="Timer")
-    check_label.config(text="")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
     global reps
     reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- #
 
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
     global reps
     reps += 1
 
-    work_seconds = WORK_MIN * 60
-    short_break_seconds = SHORT_BREAK_MIN * 60
-    long_break_seconds = LONG_BREAK_MIN * 60
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
-        count_down(long_break_seconds)
-        timer.config(text="Break", fg=RED)
-
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
     elif reps % 2 == 0:
-        count_down(short_break_seconds)
-        timer.config(text="Break", fg=PINK)
-
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
     else:
-        count_down(work_seconds)
-        timer.config(text="Working", fg=GREEN)
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-# One solution below (can't use this because of the mainloop screen "listening" for updates -> use window.after()
-# import time
-# count = 5
-# while true:
-#     time.sleep(1)
-#     count -= 5
-
-# def say_something(a, b, c):
-#     print(a)
-#     print(b)
-#     print(c)
-#
-# window.after(time_in_milliseconds, function_to_execute, *args)
-# window.after(1000, say_something, 3, 4, 6)
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    count_min = math.floor(count / 60)  # gives us number of whole minutes
-    count_sec = count % 60  # gives us remaining seconds, but at 0 seconds it says :0 instead of :00
-    # Dynamic Typing:
-    if 9 < count_sec < 60:
-        canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")  # (the item you're configuring, and the attribute)
-    elif count_sec < 10:
-        canvas.itemconfig(timer_text, text=f"{count_min}:0{count_sec}")  # (the item you're configuring, and the attribute)
 
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        global clock_timer
-        clock_timer = window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ""
         work_sessions = math.floor(reps/2)
         for _ in range(work_sessions):
             marks += "✔"
-        check_label.config(text=marks)
+        check_marks.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-# Canvas Widget -> allows us to place an image onto program and then place text on top of it
-canvas = Canvas(width=200, height=300, bg=YELLOW, highlightthickness=0)  # looked at size of tomato image
-tomato_img = PhotoImage(file="tomato.png")  # if image is stored in a diff folder, provide file path exactly
-canvas.create_image(100, 112, image=tomato_img)  # (X position, Y position, image)
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-timer = Label(text="Timer", fg=RED, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
-timer.grid(column=1, row=0)
-
-start_button = Button(text="Start", font=(FONT_NAME, 10), command=start_timer)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 10), command=reset_timer)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_label = Label(fg=RED, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 10, "bold"))
-check_label.grid(column=1, row=3)
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 
 window.mainloop()
-
